f36fcc3074a69b081ce3fb8ea2e8f26d5be043c3
abdul <abdulsk054@gmail.com>
2023-08-30 10:09:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -66,4 +66,32 @@ resource "aws_route" "default-vpc-peer-entry" {
 resource "aws_instance" "main" {
   instance_type = "t3small"
   image_id = "ami-03265a0778a880afb"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
